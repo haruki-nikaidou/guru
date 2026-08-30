a64846a5ae88b9bce1b13a6b812ace289c4b5832
Nikaidou Haruki <nharuki@pm.me>
2026-08-30 21:49:44 +0900

    add account entity

---
+++ modules/auth/src/entities/surreal/account.rs
@@ -0,0 +1,19 @@
+use newtype_record_id::table_record;
+use surrealdb_types::SurrealValue;
+
+table_record!(AccountId, "account");
+
+#[derive(Debug, Clone, SurrealValue)]
+pub struct AccountEntity {
+    pub id: AccountId,
+    pub email: String,
+    pub password_hash: String,
+    pub role: AccountRole,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, SurrealValue)]
+pub enum AccountRole {
+    Admin,
+    Maintainer,
+    Observer,
+}
