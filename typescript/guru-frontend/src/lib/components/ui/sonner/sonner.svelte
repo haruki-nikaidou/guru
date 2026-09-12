177f935b88c2de04c21d3a7ac264edb2017ea2ba
Nikaidou Haruki <nharuki@pm.me>
2026-09-12 20:56:17 +0900

    feat(frontend): set the default position of sonner to top middle

--- typescript/guru-frontend/src/lib/components/ui/sonner/sonner.svelte
+++ typescript/guru-frontend/src/lib/components/ui/sonner/sonner.svelte
@@ -7,13 +7,14 @@
 	import InfoIcon from '@lucide/svelte/icons/info';
 	import TriangleAlertIcon from '@lucide/svelte/icons/triangle-alert';
 
-	let { ...restProps }: SonnerProps = $props();
+	let { position = "top-center", ...restProps }: SonnerProps = $props();
 </script>
 
 <Sonner
 	theme={mode.current}
 	class="toaster group"
 	style="--normal-bg: var(--color-popover); --normal-text: var(--color-popover-foreground); --normal-border: var(--color-border);"
+	{position}
 	{...restProps}
 >
 	{#snippet loadingIcon()}
